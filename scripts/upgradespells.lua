c3746f611cbe208c97f070bdd8d550e4384b564e
Wil Thieme <wil.thieme@protonmail.com>
2020-11-28 07:25:36 -0500

    search all diseases

--- scripts/upgradespells.lua
+++ scripts/upgradespells.lua
@@ -137,8 +137,22 @@ function addBattle_new(nodeBattle)
 						local sNPCName = DB.getValue(nodeEntry, 'name')
 						if sNPCName then
 							sNPCName = string.lower(sNPCName:gsub('%A', ''))
-							if DB.findNode('reference.diseases@PFRPG SRD Maladies') then
+							if DB.findNode('reference.diseases@*') then
 								for _,nodeDisease in pairs(DB.findNode('reference.diseases@*').getChildren()) do
+									local sDiseaseCreature = DB.getValue(nodeDisease, 'npc')
+									if sDiseaseCreature then
+										sDiseaseCreature = string.lower(sDiseaseCreature:gsub('%A', ''))
+										if sDiseaseCreature == sNPCName then
+											local sDesc = DB.getValue(nodeEntry, 'text', '')
+											local sDiseaseName = DB.getValue(nodeDisease, 'name')
+											local sDescAdd = '<linklist><link class="referencedisease" recordname="' .. DB.getPath(nodeDisease) .. '"><b>Malady: </b>' .. sDiseaseName .. '</link></linklist>'
+											DB.setValue(nodeEntry, 'text', 'formattedtext', sDescAdd .. sDesc)										
+										end
+									end
+								end
+							end
+							if DB.findNode('disease') then
+								for _,nodeDisease in pairs(DB.findNode('disease').getChildren()) do
 									local sDiseaseCreature = DB.getValue(nodeDisease, 'npc')
 									if sDiseaseCreature then
 										sDiseaseCreature = string.lower(sDiseaseCreature:gsub('%A', ''))
