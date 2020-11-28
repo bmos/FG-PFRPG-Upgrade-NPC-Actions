557667e63666fa5f63db410b8ff42e8540cc333c
Wil Thieme <wil.thieme@protonmail.com>
2020-11-28 13:37:15 -0500

    add tooltips showing automation to effects library

---
+++ scripts/effectdescriptions.lua
@@ -0,0 +1,43 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+-- effects summaries for tooltips in effect list
+-- NOTE: From rules, missing dying, staggered and disabled
+conditionshelp = {
+	"AC:-2; GRANTCA; SKILL:-2 strength dexterity; SKILL:-4 search perception", 
+	"GRANTCA",
+	"confused",
+	"AC:-2",
+	"dazed",
+	"ATK:-1; SKILL:-1 spot search perception",
+	"INIT: -4", 
+	"ATK:-2; DEX:-4", 
+	"DEX:-6; STR:-6",
+	"SKILL:-4 spot listen perception",
+	"DEX:-2; STR:-2",
+	"flat-footed",
+	"ATK:-2; SAVE:-2; SKILL:-2; ABIL:-2", 
+	"GRANTCA", 
+	"AC:-4 melee",
+	"incorporeal", 
+	"ATK:2; CA; TCONC", 
+	"AC: -2 melee; AC: 2 ranged",
+	"nauseated",
+	"ATK:-2; SAVE:-2; SKILL:-2; ABIL:-2", 
+	"AC:-4 melee",
+	"AC:-4 melee",
+	"GRANTCA, AC:-4", 
+	"ATK:-4 melee; AC:-4 melee; AC:4 ranged", 
+	"AC:-2",
+	"GRANTCA",
+	"ATK:-2; SAVE:-2; SKILL:-2; ABIL:-2", 
+	"ATK:-2; DMG:-2; SAVE:-2; SKILL:-2; ABIL:-2", 
+	"AC: -2 melee; AC: 2 ranged",
+	"ATK:-1; AC:-1; REF:-1", 
+	"GRANTCA; ATK:-5", 
+	"stable", 
+	"AC:-2; GRANTCA",
+	"AC:-2",
+	"AC:-4 melee"
+};
