ed662da1d68b2237554f4a6526ef836e88ca8d07
Wil Thieme <wil.thieme@protonmail.com>
2021-05-17 18:22:13 -0400

    a fix for getDatabaseNode error
    I don't like this.

--- campaign/scripts/spell.lua
+++ campaign/scripts/spell.lua
@@ -3,6 +3,31 @@
 -- attribution and copyright information.
 --
 
+function onInit()
+	if not windowlist.isReadOnly() then
+		registerMenuItem(Interface.getString("menu_deletespell"), "delete", 6);
+		registerMenuItem(Interface.getString("list_menu_deleteconfirm"), "delete", 6, 7);
+
+		registerMenuItem(Interface.getString("menu_addspellaction"), "pointer", 3);
+		registerMenuItem(Interface.getString("menu_addspellcast"), "radial_sword", 3, 2);
+		registerMenuItem(Interface.getString("menu_addspelldamage"), "radial_damage", 3, 3);
+		registerMenuItem(Interface.getString("menu_addspellheal"), "radial_heal", 3, 4);
+		registerMenuItem(Interface.getString("menu_addspelleffect"), "radial_effect", 3, 5);
+		
+		registerMenuItem(Interface.getString("menu_reparsespell"), "textlist", 4);
+	end
+
+	-- Check to see if we should automatically parse spell description
+	local nodeSpell = getDatabaseNode();
+	local nParse = DB.getValue(nodeSpell, "parse", 0);
+	if nParse ~= 0 then
+		DB.setValue(nodeSpell, "parse", "number", 0);
+		SpellManager.parseSpell(nodeSpell);
+	end
+	
+	onDisplayChanged();
+end
+
 function onMenuSelection(selection, subselection)
 	if selection == 6 and subselection == 7 then
 		getDatabaseNode().delete();
@@ -24,5 +49,24 @@ function onMenuSelection(selection, subselection)
 			createAction("effect");
 			activatedetail.setValue(1);
 		end
+	end
+end
+
+function onDisplayChanged()
+	if minisheet then
+		return;
+	end
+	
+	local sDisplayMode = ""
+	if DB and getDatabaseNode then
+		sDisplayMode = DB.getValue(getDatabaseNode(), ".......spelldisplaymode", "");
+	end
+
+	if header and sDisplayMode == "action" then
+		header.subwindow.shortdescription.setVisible(false);
+		header.subwindow.actionsmini.setVisible(true);
+	elseif header then
+		header.subwindow.shortdescription.setVisible(true);
+		header.subwindow.actionsmini.setVisible(false);
 	end
 end
