ff53deff79c7bb1f8262f41497a84cb817def6c0
Wil Thieme <wil.thieme@protonmail.com>
2022-04-11 09:04:01 -0400

    linter exceptions

--- campaign/scripts/spell.lua
+++ campaign/scripts/spell.lua
@@ -1,30 +1,6 @@
 --
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-function onInit()
-	if not windowlist.isReadOnly() then
-		registerMenuItem(Interface.getString('menu_deletespell'), 'delete', 6);
-		registerMenuItem(Interface.getString('list_menu_deleteconfirm'), 'delete', 6, 7);
-
-		registerMenuItem(Interface.getString('menu_addspellaction'), 'pointer', 3);
-		registerMenuItem(Interface.getString('menu_addspellcast'), 'radial_sword', 3, 2);
-		registerMenuItem(Interface.getString('menu_addspelldamage'), 'radial_damage', 3, 3);
-		registerMenuItem(Interface.getString('menu_addspellheal'), 'radial_heal', 3, 4);
-		registerMenuItem(Interface.getString('menu_addspelleffect'), 'radial_effect', 3, 5);
-
-		registerMenuItem(Interface.getString('menu_reparsespell'), 'textlist', 4);
-	end
-
-	-- Check to see if we should automatically parse spell description
-	local nodeSpell = getDatabaseNode();
-	local nParse = DB.getValue(nodeSpell, 'parse', 0);
-	if nParse ~= 0 then
-		DB.setValue(nodeSpell, 'parse', 'number', 0);
-		SpellManager.parseSpell(nodeSpell);
-	end
-
-	onDisplayChanged();
-end
 
 --	luacheck: globals onMenuSelection
 function onMenuSelection(selection, subselection)
@@ -35,6 +11,7 @@ function onMenuSelection(selection, subselection)
 		-- bmos removing this line to keep script error away
 		-- activatedetail.setValue(1);
 	elseif selection == 3 then
+		-- luacheck: globals createAction
 		if subselection == 2 then
 			createAction('cast');
 			activatedetail.setValue(1);
@@ -53,6 +30,7 @@ end
 
 --	luacheck: globals onDisplayChanged
 function onDisplayChanged()
+	-- luacheck: globals minisheet
 	if minisheet then return; end
 
 	local sDisplayMode = ''
@@ -77,4 +55,30 @@ function onDisplayChanged()
 			if OptionsManager.isOption('SAIC', 'on') then header.subwindow.components_text_label.setVisible(false); end
 		end
 	end
+end
+
+function onInit()
+	if not windowlist.isReadOnly() then
+		-- luacheck: globals registerMenuItem
+		registerMenuItem(Interface.getString('menu_deletespell'), 'delete', 6);
+		registerMenuItem(Interface.getString('list_menu_deleteconfirm'), 'delete', 6, 7);
+
+		registerMenuItem(Interface.getString('menu_addspellaction'), 'pointer', 3);
+		registerMenuItem(Interface.getString('menu_addspellcast'), 'radial_sword', 3, 2);
+		registerMenuItem(Interface.getString('menu_addspelldamage'), 'radial_damage', 3, 3);
+		registerMenuItem(Interface.getString('menu_addspellheal'), 'radial_heal', 3, 4);
+		registerMenuItem(Interface.getString('menu_addspelleffect'), 'radial_effect', 3, 5);
+
+		registerMenuItem(Interface.getString('menu_reparsespell'), 'textlist', 4);
+	end
+
+	-- Check to see if we should automatically parse spell description
+	local nodeSpell = getDatabaseNode();
+	local nParse = DB.getValue(nodeSpell, 'parse', 0);
+	if nParse ~= 0 then
+		DB.setValue(nodeSpell, 'parse', 'number', 0);
+		SpellManager.parseSpell(nodeSpell);
+	end
+
+	onDisplayChanged();
 end
