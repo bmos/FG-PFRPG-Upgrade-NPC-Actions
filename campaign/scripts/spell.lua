c52a4c149fb2c7ba32c657359a5ff2c114b4bc7c
Wil Thieme <wil.thieme@protonmail.com>
2022-12-31 13:29:46 -0500

    stylua

--- campaign/scripts/spell.lua
+++ campaign/scripts/spell.lua
@@ -6,7 +6,7 @@ function onMenuSelection(selection, subselection, ...)
 	if super and super.onMenuSelection and selection ~= 4 then
 		super.onMenuSelection(selection, subselection, ...)
 	elseif getDatabaseNode and selection == 4 then
-		SpellManager.parseSpell(getDatabaseNode());
+		SpellManager.parseSpell(getDatabaseNode())
 		-- activatedetail.setValue(1); -- remove line to keep script error away
 	end
 end
@@ -14,28 +14,28 @@ end
 --	luacheck: globals onDisplayChanged
 function onDisplayChanged()
 	-- luacheck: globals minisheet
-	if minisheet then return; end
+	if minisheet then return end
 
 	local sDisplayMode = ''
-	if DB and getDatabaseNode then sDisplayMode = DB.getValue(getDatabaseNode(), '.......spelldisplaymode', ''); end
+	if DB and getDatabaseNode then sDisplayMode = DB.getValue(getDatabaseNode(), '.......spelldisplaymode', '') end
 
 	if header and sDisplayMode == 'action' then
-		header.subwindow.shortdescription.setVisible(false);
-		header.subwindow.actionsmini.setVisible(true);
+		header.subwindow.shortdescription.setVisible(false)
+		header.subwindow.actionsmini.setVisible(true)
 		-- add compatibility with Zarestia's  Spell casting time labels extension
 		-- adds display change so casting time not shown in summary display
 		if header.subwindow.action_text_label and header.subwindow.components_text_label then
-			header.subwindow.action_text_label.setVisible(true);
-			if OptionsManager.isOption('SAIC', 'on') then header.subwindow.components_text_label.setVisible(true); end
+			header.subwindow.action_text_label.setVisible(true)
+			if OptionsManager.isOption('SAIC', 'on') then header.subwindow.components_text_label.setVisible(true) end
 		end
 	elseif header then
-		header.subwindow.shortdescription.setVisible(true);
-		header.subwindow.actionsmini.setVisible(false);
+		header.subwindow.shortdescription.setVisible(true)
+		header.subwindow.actionsmini.setVisible(false)
 		-- add compatibility with Zarestia's  Spell casting time labels extension
 		-- adds display change so casting time not shown in summary display
 		if header.subwindow.action_text_label and header.subwindow.components_text_label then
-			header.subwindow.action_text_label.setVisible(false);
-			if OptionsManager.isOption('SAIC', 'on') then header.subwindow.components_text_label.setVisible(false); end
+			header.subwindow.action_text_label.setVisible(false)
+			if OptionsManager.isOption('SAIC', 'on') then header.subwindow.components_text_label.setVisible(false) end
 		end
 	end
 end
@@ -43,5 +43,5 @@ end
 function onInit()
 	if super and super.onInit then super.onInit() end
 
-	onDisplayChanged();
+	onDisplayChanged()
 end
