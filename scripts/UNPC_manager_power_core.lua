925111f0fd46f6fcbefb03b8b118fe26ce5c3a00
Wil Thieme <wil.thieme@protonmail.com>
2023-03-16 16:06:57 -0400

    stylua

--- scripts/UNPC_manager_power_core.lua
+++ scripts/UNPC_manager_power_core.lua
@@ -5,23 +5,19 @@
 --	luacheck: globals onDefaultPowerMenuSelection
 function onDefaultPowerMenuSelection(w, selection, subselection)
 	if selection == 6 and subselection == 7 then
-		DB.deleteNode(w.getDatabaseNode());
+		DB.deleteNode(w.getDatabaseNode())
 	elseif selection == 4 then
-		PowerManagerCore.parsePower(w.getDatabaseNode());
+		PowerManagerCore.parsePower(w.getDatabaseNode())
 		if type(w) ~= 'error' and w.activatedetail then
-			w.activatedetail.setValue(1); -- remove line to keep script error away
+			w.activatedetail.setValue(1) -- remove line to keep script error away
 		end
 	elseif selection == 3 then
-		local tTypes = PowerActionManagerCore.getSortedActionTypes();
-		local nBaseIndex = PowerManagerCore.getDefaultPowerMenuBaseIndex(tTypes);
-		local nActionIndex = ((subselection - nBaseIndex) % 8) + 1;
-		local sType = tTypes[nActionIndex];
-		if sType then
-			PowerManagerCore.createPowerAction(w, sType);
-		end
+		local tTypes = PowerActionManagerCore.getSortedActionTypes()
+		local nBaseIndex = PowerManagerCore.getDefaultPowerMenuBaseIndex(tTypes)
+		local nActionIndex = ((subselection - nBaseIndex) % 8) + 1
+		local sType = tTypes[nActionIndex]
+		if sType then PowerManagerCore.createPowerAction(w, sType) end
 	end
 end
 
-function onInit()
-	PowerManagerCore.onDefaultPowerMenuSelection = onDefaultPowerMenuSelection
-end
+function onInit() PowerManagerCore.onDefaultPowerMenuSelection = onDefaultPowerMenuSelection end
