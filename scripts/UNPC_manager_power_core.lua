8b202995e96ded1789cd543ccdf029a0fef858e1
Wil Thieme <wil.thieme@protonmail.com>
2023-04-12 16:21:40 -0400

    script error fix on parse

--- scripts/UNPC_manager_power_core.lua
+++ scripts/UNPC_manager_power_core.lua
@@ -2,22 +2,18 @@
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
---	luacheck: globals onDefaultPowerMenuSelection
-function onDefaultPowerMenuSelection(w, selection, subselection)
-	if selection == 6 and subselection == 7 then
-		DB.deleteNode(w.getDatabaseNode())
-	elseif selection == 4 then
-		PowerManagerCore.parsePower(w.getDatabaseNode())
-		if type(w) ~= 'error' and w.activatedetail then
-			w.activatedetail.setValue(1) -- remove line to keep script error away
-		end
-	elseif selection == 3 then
-		local tTypes = PowerActionManagerCore.getSortedActionTypes()
-		local nBaseIndex = PowerManagerCore.getDefaultPowerMenuBaseIndex(tTypes)
-		local nActionIndex = ((subselection - nBaseIndex) % 8) + 1
-		local sType = tTypes[nActionIndex]
-		if sType then PowerManagerCore.createPowerAction(w, sType) end
+--	luacheck: globals onDefaultPowerMenuSelection_new
+local onDefaultPowerMenuSelection_old
+function onDefaultPowerMenuSelection_new(w, selection, subselection, ...)
+	if selection ~= 4 then return onDefaultPowerMenuSelection_old(w, selection, subselection, ...) end
+
+	PowerManagerCore.parsePower(w.getDatabaseNode())
+	if type(w) ~= 'error' and w.activatedetail then
+		w.activatedetail.setValue(1);
 	end
 end
 
-function onInit() PowerManagerCore.onDefaultPowerMenuSelection = onDefaultPowerMenuSelection end
+function onInit()
+	onDefaultPowerMenuSelection_old = PowerManagerCore.onDefaultPowerMenuSelection
+	PowerManagerCore.onDefaultPowerMenuSelection = onDefaultPowerMenuSelection_new
+end
